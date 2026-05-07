d9bd4dd8c0f3768131a3d80b6c0c3dc191b5aaa5
Godswill <iyiegodswill@gmail.com>
2026-05-07 16:06:33 +0100

    JavaApplicationSeptMMS2025

---
+++ EscapeCharacater.java
@@ -0,0 +1,11 @@
+public class EscapeCharacater{
+	public static void main(String[] args) {
+		System.out.print("When you need to use the escape characters \n ");
+		System.out.print("Use it when you want to manipulate your cursor");
+		System.out.print("Use when you want a newline \n");
+		System.out.print("Who is the father of java?. The father of \"java is James Gosling\"\n");
+		System.out.print("\tUse this when you want a tab or indentation\r");
+		System.out.print("We are using carriage return here");
+	}
+	
+}
